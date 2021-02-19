1bd41fc35f04f96dc2fbf877aa489b0b3cee44fe
BookCloud <some0choo@gmail.com>
2021-02-19 21:26:27 +0800

    changed a lot of stuff... fixed whitespace issue

--- mainbot.py
+++ mainbot.py
@@ -6,76 +6,70 @@
 from telethon import TelegramClient, events
 import logging
 import json
+import re
 
+#load in keywords
 with open("text/keywords.json") as json_file:
-    data = json.load(json_file)
-    keyWord = data["keywords"]
-    editedKeywords = []
-    for i in range(len(keyWord)):
-        new = keyWord[i].upper()
-        editedKeywords.append(new)
+    keyWords = [i.upper() for i in json.load(json_file)["keywords"]]
+    print("\n Keywords: ", keyWords)
 
-    print(editedKeywords)
-
+#load in blacklist
 with open("text/blacklist.json") as json_file:
-    data = json.load(json_file)
-    black = data["black"]
-    editedBlack = []
-    for i in range(len(black)):
-        new = black[i].upper()
-        editedBlack.append(new)
+    black = [i.upper() for i in json.load(json_file)["black"]]
+    print("\n Black listed words: ", black)
 
-    print(editedBlack)
-
+#load in currencies
 with open("text/currencies.json") as json_file:
-    data = json.load(json_file)
-    currency = data["currency"]
-    editedCurr = []
-    for i in range(len(currency)):
-        new = currency[i].lower()
-        editedCurr.append(new)
+    currency = [i.upper() for i in json.load(json_file)["currency"]]
+    print("\n Currencies: ", currency)
 
-    print(editedCurr)
+with open("text/caps.json") as json_file:
+    caps = [i.upper() for i in json.load(json_file)["caps"]]
+    caps = caps + currency
+    print("\n Caps: ", caps)
 
 
+#load in publishers
 with open("text/receiveFrom.json") as json_file:
-    data = json.load(json_file)
-    skemmers = data["skemmerMan"]
-    print(skemmers)
+    skemmers = json.load(json_file)["skemmerMan"]
+    print("\n WOlfs: ", skemmers)
 
-
+#load in subscribers
 with open("text/sendTo.json") as json_file:
-    data = json.load(json_file)
-    sheeps = data["sheeple"]
-    print(sheeps)
+    sheeps = json.load(json_file)["sheeple"]
+    print("\n sheeps", sheeps)
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
 )
-print("Bot Running...")
+
+print("\nBot Running...")
 # Use your own values from my.telegram.org
 api_id = 2388554
 api_hash = "3e30cf236573b82381da92246f9d9586"
-
-
 client = TelegramClient("anon", api_id, api_hash)
+
+# listen to publishers
 
 
 @client.on(events.NewMessage(from_users=skemmers))
 async def my_event_handler(event):
-    msg = event.raw_text
-    txt = msg.upper()
-    print(msg)
-    if [ele for ele in editedKeywords if (ele in txt)]:
-        if not [j for j in editedBlack if (j in txt)]:
-            for i in editedCurr:
-                msg = ' '.join(
-                    [i.upper() if idx.lower() == i else idx for idx in msg.split()])
+    rawMsg = event.raw_text  # message receive
+    newMsg = rawMsg  # creates another instance to store message to edit later on
+    print(rawMsg)
+    # checks if message contains keywords and currency [i for i in keyWords if re.search(i, rawMsg, re.IGNORECASE)] and
+    if[o for o in currency if re.search(o, rawMsg, re.IGNORECASE)]:
+        # checks if message does not contain blacklisted words
+        if not [j for j in black if re.search(j, rawMsg, re.IGNORECASE)]:
+            # Caps the currency pairs
+            for k in caps:
+                newMsg = re.sub(k, k.upper(), newMsg, flags=re.IGNORECASE)
 
-            print("send")
-            for x in sheeps:
-                await client.send_message(x, msg)
+        # Send message to suscribers
+        print("Sending\n")
+        for x in sheeps:
+            await client.send_message(x, newMsg)
 
-
+# Starts and run session until disconnected
 client.start()
 client.run_until_disconnected()
--- test.py
+++ test.py
@@ -2,13 +2,23 @@ import re
 
 res = "apple banana peach pear grape melon yes no shu yun"
 ls = ["apple", "pear", "grape", "shu", "heart"]
+bl = ["gold", "sell"]
+f = open("test.txt", "r")
+data = f.read()
+# print(data)
+k = ["apple", "pear"]
+sub = "GOLD"
+#x = re.sub(k[0], sub, data, flags=re.IGNORECASE)
+#x = re.sub(k[1], sub, x, flags=re.IGNORECASE)
 
+# print(x)
+n = data
 
-# Replace multiple words with K
-# Using join() + split() + list comprehension
+if [x for x in ls if re.search(x, data, re.IGNORECASE)]:
+    if not[i for i in bl if re.search(i, data, re.IGNORECASE)]:
+        for j in ls:
+            n = re.sub(j, j.upper(), n, flags=re.IGNORECASE)
+            print(n)
+        print(n)
 
-for i in ls:
-    print(i)
-    res = ' '.join(
-        [i.upper() if idx == i else idx for idx in res.split()])
-    print(res)
+f.close()
